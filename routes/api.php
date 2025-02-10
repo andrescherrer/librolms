0326b9510cf4c85e79616ff1216584c8c321d381
André C. Scherrer <andrescherrer@gmail.com>
2025-02-10 14:18:49 -0300

    feat: adiciona rota /cursos

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\CursoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function() {
+    Route::apiResource('cursos', CursoController::class);
+});
