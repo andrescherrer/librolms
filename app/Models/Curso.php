8faae1d675600f2a05395222e3a2dc05eed15f38
André C. Scherrer <andrescherrer@gmail.com>
2025-02-10 14:11:09 -0300

    feat: adiciona trait HasFactory

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
+    use HasFactory;
+    
     protected $fillable = ['titulo', 'descricao'];
 }
