0db60928fe34a26aee028a108b32c7c702281efb
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 05:43:00 -0300

    feat: atualiza Model Curso

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -10,4 +10,19 @@ class Curso extends Model
     use HasFactory;
     
     protected $fillable = ['titulo', 'descricao'];
+
+    public function matriculas()
+    {
+        return $this->hasMany(Matricula::class);
+    }
+
+    public function alunos()
+    {
+        return $this->belongsToMany(Aluno::class, 'matriculas');
+    }
+
+    public function getDescricaoTruncadaAttribute()
+    {
+        return strlen($this->descricao) > 100 ? substr($this->descricao, 0, 100) . '...' : $this->descricao;
+    }
 }
