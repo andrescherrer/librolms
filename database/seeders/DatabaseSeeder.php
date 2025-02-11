c3b709acee8291824b7ab9b8db1eab6f9d3c3231
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 06:26:53 -0300

    feat: atualiza DatabaseSeeder chamando MatriculaSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([            
             CursoSeeder::class,
             AlunoSeeder::class,
+            MatriculaSeeder::class,
         ]);
     }
 }
