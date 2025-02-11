d98ec0259906eb76c41a008379faeeaec3f7d6c3
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 20:58:57 -0300

    feat: adiciona rota para exibir alunos pelos por sexo e curso

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ Route::prefix('v1')->group(function() {
     Route::apiResource('cursos', CursoController::class);
     Route::apiResource('alunos', AlunoController::class);
     Route::apiResource('matriculas', MatriculaController::class);
+    Route::get('/listar', [MatriculaController::class, 'alunos_por_faixa_etaria_por_curso_e_sexo']);
 });
