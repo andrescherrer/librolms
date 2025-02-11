e32401a653fc96a307686e3a3092395882db5ae3
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 06:59:31 -0300

    feat: adiciona Service MatriculaService

---
+++ app/Services/MatriculaService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Matricula;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Log;
+
+class MatriculaService extends Service
+{
+    public function __construct(
+        private Matricula $model,
+    ) {}
+    public function filter(Request $request): LengthAwarePaginator
+    {
+        return Matricula
+            ::with(
+                ['aluno', 'curso']
+            )
+            ->whereHas('aluno', function(Builder $query) use ($request) {
+                $this->aluno($request, $query);
+            })
+            ->whereHas('curso', function(Builder $query) use ($request) {
+                $this->curso($request, $query);
+            })
+            ->orderBy('id')
+            ->paginate();
+    }
+
+    public function create(Request $request): bool
+    {
+        try {
+            $this->model->create($request->all());
+            return true;
+        }
+        catch (\Exception $e) {
+            Log::critical("Erro ao salvar o registro: ". $e->getMessage());
+            return false;
+        }        
+    }
+
+    public function update(Matricula $matricula, Request $request): bool
+    {
+        return $matricula->update($request->validated());
+    }
+
+    private function aluno($request, $query)
+    {
+        if ($request->filled('nome')) {
+            $query->where('nome', 'LIKE', "%{$request->input('nome')}%");
+        }
+        if ($request->filled('email')) {
+            $query->where('email', 'LIKE', "%{$request->input('email')}%");
+        }        
+    }
+    private function curso($request, $query)
+    {
+        if ($request->filled('titulo')) {
+            $query->where('titulo', 'LIKE', "%{$request->input('titulo')}%");
+        }
+        if ($request->filled('descricao')) {
+            $query->where('descricao', 'LIKE', "%{$request->input('descricao')}%");
+        }
+    }
+}
