cd04e90d712ceeb82a2fc4ea501b01c0511d2dff
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 06:26:15 -0300

    feat: adiciona MatriculaSeeder

---
+++ database/seeders/MatriculaSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MatriculaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $combinations = [];
+        $createdAt = $updatedAt = now();
+
+        $id = 1;
+        for ($alunoId = 1; $alunoId <= 50; $alunoId++) {
+            for ($cursoId = 1; $cursoId <= 10; $cursoId++) {
+                $combinations[] = [
+                    "id" => $id,
+                    "aluno_id" => $alunoId,
+                    "curso_id" => $cursoId,
+                    "created_at" => $createdAt,
+                    "updated_at" => $updatedAt
+                ];
+                $id++;
+            }
+        }
+
+        DB::table("matriculas")->insert(
+            $combinations
+        );
+    }
+}
