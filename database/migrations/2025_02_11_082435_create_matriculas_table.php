efe50db9e5d432865496a5069a72096e733a0908
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 05:43:34 -0300

    feat: adiciona arquivo de migracao tabela matriculas

---
+++ database/migrations/2025_02_11_082435_create_matriculas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatriculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matriculas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('aluno_id')->constrained('alunos')->onDelete('cascade');
+            $table->foreignId('curso_id')->constrained('cursos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matriculas');
+    }
+}
