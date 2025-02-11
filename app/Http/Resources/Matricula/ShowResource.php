509a3437a82b2e16320fd881461226483040f542
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 07:21:51 -0300

    feat: adiciona Resource Matricula/ShowResource

---
+++ app/Http/Resources/Matricula/ShowResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Matricula;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'aluno' => $this->aluno,
+            'curso' => $this->curso,
+            'created' => $this->created_at,
+            'updated' => $this->updated_at,            
+        ];        
+    }
+}
