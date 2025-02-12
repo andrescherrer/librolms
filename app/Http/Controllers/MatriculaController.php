8d09b4ab5dc59af5772586a9c90bdd465edb92ef
André C. Scherrer <andrescherrer@gmail.com>
2025-02-11 21:10:23 -0300

    feat: adiciona metodo alunos_por_faixa_etaria_por_curso_e_sexo

--- app/Http/Controllers/MatriculaController.php
+++ app/Http/Controllers/MatriculaController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Matricula\{IndexRequest,StoreRequest, UpdateRequest};
 use App\Http\Resources\Matricula\IndexCollection;
 use App\Http\Resources\Matricula\ShowResource;
+use App\Models\Aluno;
 use App\Models\Matricula;
 use App\Services\MatriculaService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class MatriculaController extends Controller
 {
@@ -53,5 +55,25 @@ class MatriculaController extends Controller
         $matricula->delete();
 
         return response()->json(['message' => 'Matrícula excluída com sucesso'], JsonResponse::HTTP_NO_CONTENT);
+    }
+
+    public function alunos_por_faixa_etaria_por_curso_e_sexo()
+    {
+        $dados = DB::table('matriculas as m')
+            ->join('alunos as a', 'a.id', '=', 'm.aluno_id')
+            ->join('cursos as c', 'c.id', '=', 'm.curso_id')
+            ->select(
+                'c.titulo',
+                'a.sexo',
+                DB::raw('COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, a.nascimento, CURDATE()) < 15 THEN 1 END) AS menor_que_15'),
+                DB::raw('COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, a.nascimento, CURDATE()) BETWEEN 15 AND 18 THEN 1 END) AS entre_15_e_18'),
+                DB::raw('COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, a.nascimento, CURDATE()) BETWEEN 19 AND 24 THEN 1 END) AS entre_19_e_24'),
+                DB::raw('COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, a.nascimento, CURDATE()) BETWEEN 25 AND 30 THEN 1 END) AS entre_25_e_30'),
+                DB::raw('COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, a.nascimento, CURDATE()) > 30 THEN 1 END) AS maior_que_30')
+            )
+            ->groupBy('c.titulo', 'a.sexo')
+            ->get();
+
+            return response()->json($dados);
     }
 }
